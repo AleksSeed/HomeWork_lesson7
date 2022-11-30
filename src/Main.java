ea894d0b9267620fcdb14820069bc7b8cc989e9f
SAlekS <sas431@mail.ru>
2022-11-30 15:42:07 +0300

    HomeWork2

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
         }
         System.out.println("Средняя сумма трат за месяц составила " + (average / arr.length) + " рублей");
 
-        System.out.println("\nЗадание 3");
+        System.out.println("\nЗадание 4");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
